0763ef727ecd0d51a360c88acd9a0a09e4607263
Sergey Potudansky <misternort@gmail.com>
2018-02-25 00:46:29 +0300

    Vanya durak

--- weather/Program.cs
+++ weather/Program.cs
@@ -26,18 +26,34 @@ namespace weather
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-                string DiaDiem = "Moskva";
+                string DiaDiem = "Moscow";
                 string key = "3dcf4cec81ccab8e5fa391ef6afa7a40";
-                HttpResponseMessage respon = await client.GetAsync("?q=" +DiaDiem+ "&APPID=" +key);
+                HttpResponseMessage respon = await client.GetAsync("?q=" + DiaDiem + "&APPID=" + key);
                 if(respon.StatusCode == HttpStatusCode.OK)
                 {
                     //string kq = await respon.Content.ReadAsStringAsync();
                     //Console.WriteLine(kq);
                     OpenWeather w = respon.Content.ReadAsAsync<OpenWeather>().Result;
+                    string prevTemp = w.main.temp;
+                    string temp = "";
+                    for (int i = 0; i < prevTemp.Length; ++i)
+                    {
+                        if (prevTemp[i] == '.')
+                        {
+                            temp += ',';
+                        } else
+                        {
+                            temp += prevTemp[i];
+                        }
+                    }
+                    double doubleTemp = Convert.ToDouble(temp);
+                    doubleTemp -= 273.15;
+                    temp = Convert.ToString(doubleTemp);
                     Console.WriteLine("ID dia diem: "+ w.id);
                     Console.WriteLine("Dia diem: " + w.name);
+                    Console.WriteLine("Temperatura (C): " + temp);
                     Console.WriteLine("Temperatura (F): " + w.main.temp);
-                    Console.WriteLine("Vlajnost`:" + w.main.humidity);
+                    Console.WriteLine("Humidity:" + w.main.humidity);
                     Console.WriteLine("Thoi tiet: " + w.weather[0].main + " - " + w.weather[0].iddescription);
                 }
                 Console.ReadKey();
